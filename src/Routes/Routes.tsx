a136911c394b724fe4deb85f970822e37c80b6ec
Darkuus <vitork42@gmail.com>
2020-09-17 23:56:36 -0300

    commit correcoes

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 import { Route, Router, withRouter, RouteComponentProps } from 'react-router-dom';
 
 import Balanced from '../Pages/balanced';
-import Deck from '../Pages/Deck';
 
 const Routes: React.FC = () => (
     <Route>
-        <Route component={ Balanced } path="/Balanced" />
-        <Route component={ Deck } path="/Deck" />
         <Route component={ Balanced } path="/" exact/>
     </Route>
 );
